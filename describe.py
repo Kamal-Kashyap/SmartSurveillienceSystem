21e478d8d380a34f20d3649e37759539a49b31a1
Kamal-Kashyap <96367273+Kamal-Kashyap@users.noreply.github.com>
2022-10-29 23:37:41 +0530

    Delete describe.py

--- describe.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-connection = sqlite3.connect('Detailss.db')
-cursor = connection.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Shows
-              (fname,dat,time,street,colony,city,Nearest_Police_Station,Mobile_number,Email,Description_of_incident )''')
-connection.commit()
-connection.close()
